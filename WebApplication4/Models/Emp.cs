e0212fb655a8a60f28800a62752bb5197004987a
anasfatima562@gmail.com <anasfatima562@gmail.com>
2021-03-16 10:53:39 +0530

    second commit

---
+++ WebApplication4/Models/Emp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class Emp
+    {
+
+        public int Eid { get; set; }
+        public string Ename { get; set; }
+        public string EDesig { get; set; }
+        public DateTime EDOJ { get; set; }
+    }
+}
